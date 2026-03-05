d3b9b9fba05ed414518d662b5f7de9d85472c7b9
ganesh <ganeshaktegar23@gmail.com>
2026-03-05 11:33:16 +0530

    added seperate models for each device type(fan,motor_bulb)

--- train_model.py
+++ train_model.py
@@ -10,21 +10,21 @@ import matplotlib.pyplot as plt
 data = pd.read_csv("sensors_dataset_training.csv")
 
 
-# Example: Train model for Motor fault prediction
+#Model data for motor
 motor_X = data[["motor_current","motor_voltage","motor_temp","motor_vibration"]]
 motor_y = data["motor_fault"]
 
-
+#Model data for Fan
 fan_x = data[["fan_current", "fan_voltage", "fan_temp", "fan_vibration"]]
 fan_y = data["fan_fault"]
 
-
+#Model data for Bulb
 bulb_x = data[["bulb_current", "bulb_voltage", "bulb_temp"]]
 bulb_y = data["bulb_fault"]
 
 
 
-
+#Train and generate models
 def creating_model(x,y):
 
     x_train, x_test, y_train, y_test = train_test_split(
@@ -46,11 +46,13 @@ def creating_model(x,y):
     return model
 
 
+#Creating Models
 motor_model = creating_model(motor_X,motor_y)
 fan_model = creating_model(fan_x,fan_y)
 bulb_model = creating_model(bulb_x,bulb_y)
 
 
+#Saving models into a file
 for i in ["motor_model","fan_model","bulb_model"]:
     joblib.dump(i,f"rfm_{i}.pkl")
 
